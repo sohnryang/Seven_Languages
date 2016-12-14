42812939b0cfbbba191c981c77d8722b3b8cc642
sohnryang <ryang030528@gmail.com>
2016-12-14 22:52:18 +0900

    Changed RubyGrep to use File.each_line

--- Ruby/rubygrep.rb
+++ Ruby/rubygrep.rb
@@ -1,6 +1,6 @@
 file_name = ARGV[0]
 regex = Regexp.new ARGV[1]
-File.read(file_name).each_line do |line|
+File.new(file_name).each_line do |line|
     if (ARGV[1] =~ regex) == 0
         puts line
     end
